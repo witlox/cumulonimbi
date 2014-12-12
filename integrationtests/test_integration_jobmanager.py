b67ac118996be62028d0dd61c9b4a81e09a7cfc7
DJBnJack <johannes.bertens@gmail.com>
2014-12-12 14:33:20 +0100

    Moved tests again Added integrationtests folder Updated .travis.yml config

---
+++ integrationtests/test_integration_jobmanager.py
@@ -0,0 +1,11 @@
+__author__ = 'Johannes'
+
+from JobManager.Repository import JobManagerRepository
+
+from mock import patch
+import unittest
+
+
+class JobManagerIntegrationTests(unittest.TestCase):
+        def test_insert_job(self):
+            self.fail()
