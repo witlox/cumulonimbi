c0739414f552414e8384ee922ffa7fc6e1b5b4f0
Johannes <johannes.bertens@gmail.com>
2015-01-30 20:39:22 +0100

    Fix to relative path. Not great, but should work in 2.7 and 3.x

--- job_manager/api.py
+++ job_manager/api.py
@@ -1,3 +1,6 @@
+if __name__ == '__main__' and __package__ is None:
+    from os import sys, path
+    sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 
 __author__ = 'Johannes'
 
