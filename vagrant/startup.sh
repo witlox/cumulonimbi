e5043fa04357a18b0350c57bdc76f953fdf1a503
Johannes <johannes.bertens@gmail.com>
2015-02-23 23:33:33 +0100

    It was too fast after starting the previous docker!
    Now there is a sleep of 15 seconds between starting cumulunimbi and the integration tests. This should be changed to an active-wait (get on /jobs?) in the future.

--- vagrant/startup.sh
+++ vagrant/startup.sh
@@ -8,14 +8,17 @@ if [ "$count" -gt 0 ]; then
 fi
 
 echo "Getting and starting the containers"
-docker pull pblittle/docker-logstash > /dev/null && echo "\tLogstash pull  OK" || echo "\tLogstack Failed"
-docker run -d --net=host --name="elk" pblittle/docker-logstash > /dev/null && echo "\tLogstash run   OK" || echo "\tLogstack Failed"
+#docker pull pblittle/docker-logstash > /dev/null && echo "\tLogstash pull  OK" || echo "\tLogstack Failed"
+#docker run -d --net=host --name="elk" pblittle/docker-logstash > /dev/null && echo "\tLogstash run   OK" || echo "\tLogstack Failed"
 
 docker pull djbnjack/mongobase > /dev/null && echo "\tMongobase pull OK" || echo "\tMongobase Failed"
 docker run -d --net=host --name="mongodb" djbnjack/mongobase > /dev/null && echo "\tMongobase run  OK" || echo "\tMongobase Failed"
 
 docker pull djbnjack/jmdocker > /dev/null && echo "\tJMDocker pull  OK" || echo "\tJMDocker Failed"
 docker run -d --net=host --name="jobmanager" djbnjack/jmdocker python cumulonimbi/job_manager/api.py > /dev/null && echo "\tJMDocker run   OK" || echo "\tJMDocker Failed"
+
+echo "Waiting 15 seconds for the api to start"
+sleep 15
 
 echo "Running integration tests"
 docker run -i --rm --net=host --name="integrationtests" djbnjack/jmdocker nosetests cumulonimbi/tests/integrationtests
