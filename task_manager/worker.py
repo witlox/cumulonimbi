66eaa0da6c09344ea6501af29f3b7d0264840c4b
Johannes <johannes.bertens@gmail.com>
2015-04-27 17:48:38 +0200

    Rewrote worker to a StoppableThread interface

--- task_manager/worker.py
+++ task_manager/worker.py
@@ -1,84 +1,106 @@
 from random import randint
+from threading import Thread
+import threading
 import time
 import zmq
 import logging
 from settings import Settings
 
 
-class Worker():
+class StoppableThread(Thread):
+    """
+    Thread class with a stop() method. The thread itself has to check regularly for the stopped() condition.
+    """
+
     def __init__(self):
-        HEARTBEAT_LIVELINESS = 3
-        HEARTBEAT_INTERVAL = 1
-        INTERVAL_INIT = 1
-        INTERVAL_MAX = 32
+        super(StoppableThread, self).__init__()
+        self._stop = threading.Event()
 
-        # Paranoid Pirate Protocol constants
-        PPP_READY = '\x01'  # Signals worker is ready
-        PPP_HEARTBEAT = '\x02'  # Signals worker heartbeat
+    def stop(self):
+        self._stop.set()
+
+    def stopped(self):
+        return self._stop.isSet()
 
 
-        def worker_socket(settings, context, poller):
-            """
-            Helper function that returns a new configured socket connected to the Paranoid Pirate queue
-            """
-            worker = context.socket(zmq.DEALER)
-            identity = '%04X-%04X' % (randint(0, 0x10000), randint(0, 0x10000))
-            worker.setsockopt(zmq.IDENTITY, identity)
-            poller.register(worker, zmq.POLLIN)
-            worker.connect('tcp://%s:%d' % (settings.job_manager_api, settings.job_manager_router_port))
-            worker.send(PPP_READY)
-            return worker
+class Worker(StoppableThread):
+    HEARTBEAT_LIVELINESS = 3
+    HEARTBEAT_INTERVAL = 1
+    INTERVAL_INIT = 1
+    INTERVAL_MAX = 32
 
-        settings = Settings()
-        settings.configure_logging('../logs/task_manager.log')
+    # Paranoid Pirate Protocol constants
+    PPP_READY = '\x01'  # Signals worker is ready
+    PPP_HEARTBEAT = '\x02'  # Signals worker heartbeat
 
-        context = zmq.Context.instance()
-        poller = zmq.Poller()
+    def worker_socket(self, settings, context, poller):
+        """
+        Helper function that returns a new configured socket connected to the Paranoid Pirate queue
+        """
+        worker = context.socket(zmq.DEALER)
+        identity = '%04X-%04X' % (randint(0, 0x10000), randint(0, 0x10000))
+        worker.setsockopt(zmq.IDENTITY, identity)
+        poller.register(worker, zmq.POLLIN)
+        worker.connect('tcp://%s:%d' % (settings.job_manager_api, settings.job_manager_router_port))
+        worker.send(self.PPP_READY)
+        return worker
 
-        liveliness = HEARTBEAT_LIVELINESS
-        interval = INTERVAL_INIT
+    def __init__(self):
+        StoppableThread.__init__(self)
+        self.daemon = True
 
-        heartbeat_at = time.time() + HEARTBEAT_INTERVAL
+        self.settings = Settings()
+        self.settings.configure_logging('../logs/task_manager.log')
 
-        worker = worker_socket(settings, context, poller)
+        self.context = zmq.Context.instance()
+        self.poller = zmq.Poller()
 
-        while True:
-            socks = dict(poller.poll(HEARTBEAT_INTERVAL * 1000))
+
+    def run(self):
+        liveliness = self.HEARTBEAT_LIVELINESS
+        interval = self.INTERVAL_INIT
+
+        heartbeat_at = time.time() + self.HEARTBEAT_INTERVAL
+
+        worker = self.worker_socket(self.settings, self.context, self.poller)
+
+        while not self.stopped():
+            socks = dict(self.poller.poll(self.HEARTBEAT_INTERVAL * 1000))
             # Handle worker activity on backend
             if socks.get(worker) == zmq.POLLIN:
-                #  Get message
-                #  - 3-part envelope + content -> request
-                #  - 1-part HEARTBEAT -> heartbeat
+                # Get message
+                # - 3-part envelope + content -> request
+                # - 1-part HEARTBEAT -> heartbeat
                 frames = worker.recv_multipart()
                 if not frames:
                     break  # Interrupted
                 if len(frames) == 3:
                     worker.send_multipart(frames)
-                    liveliness = HEARTBEAT_LIVELINESS
+                    liveliness = self.HEARTBEAT_LIVELINESS
                     time.sleep(1)  # Do some heavy work
-                elif len(frames) == 1 and frames[0] == PPP_HEARTBEAT:
+                elif len(frames) == 1 and frames[0] == self.PPP_HEARTBEAT:
                     logging.debug('Queue heartbeat')
-                    liveliness = HEARTBEAT_LIVELINESS
+                    liveliness = self.HEARTBEAT_LIVELINESS
                 else:
                     logging.info('received message: %s' % frames)
-                interval = INTERVAL_INIT
+                interval = self.INTERVAL_INIT
             else:
                 liveliness -= 1
                 if liveliness == 0:
                     logging.warn('Heartbeat failure, cannot reach queue, reconnecting in %s' % str(interval))
                     time.sleep(interval)
-                    if interval < INTERVAL_MAX:
+                    if interval < self.INTERVAL_MAX:
                         interval *= 2
-                    poller.unregister(worker)
+                    self.poller.unregister(worker)
                     worker.setsockopt(zmq.LINGER, 0)
                     worker.close()
-                    worker = worker_socket(settings, context, poller)
-                    liveliness = HEARTBEAT_LIVELINESS
+                    worker = self.worker_socket(self.settings, self.context, self.poller)
+                    liveliness = self.HEARTBEAT_LIVELINESS
             if time.time() > heartbeat_at:
-                heartbeat_at = time.time() + HEARTBEAT_INTERVAL
+                heartbeat_at = time.time() + self.HEARTBEAT_INTERVAL
                 logging.debug('Worker heartbeat')
-                worker.send(PPP_HEARTBEAT)
+                worker.send(self.PPP_HEARTBEAT)
 
         # cleanup context
         worker.close()
-        context.term()
+        self.context.term()
--- tests/unittests/test_unit_taskmanager_worker.py
+++ tests/unittests/test_unit_taskmanager_worker.py
@@ -3,8 +3,16 @@ from task_manager.worker import Worker
 
 
 class TestRepository(unittest.TestCase):
+    def setUp(self):
+        self.worker = Worker()
+        self.worker.start()
+
+    def tearDown(self):
+        self.worker.stop()
+
     def test_worker_init(self):
         pass
+
 
 if __name__ == '__main__':
     unittest.main()
