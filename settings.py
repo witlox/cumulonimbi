890f86ea99fcc4b7f9a3cf60674ce99f75e700e0
Johannes Bertens <johannes.bertens@live.nl>
2015-08-30 00:01:54 +0200

    Removed Config.json - fixed it in code

--- settings.py
+++ settings.py
@@ -44,18 +44,19 @@ class Settings(object):
     try:
         with open("../Config.json") as json_data_file:
             data = json.load(json_data_file)
+        print data
+        azure_queue_account = data['azure']['queue']['account']
+        azure_queue_key = data['azure']['queue']['key']
+        azure_topic_namespace = data['azure']['topic']['namespace']
+        azure_topic_keyname = data['azure']['topic']['keyname']
+        azure_topic_key = data['azure']['topic']['key']
     except IOError as e:
-        #Fallback config
-        with open("Config.json") as json_data_file:
-            data = json.load(json_data_file)
+        azure_queue_account = 'abc'
+        azure_queue_key = 'abc'
+        azure_topic_namespace = 'abc'
+        azure_topic_keyname = 'abc'
+        azure_topic_key = 'abc'
 
-    print data
-
-    azure_queue_account = data['azure']['queue']['account']
-    azure_queue_key = data['azure']['queue']['key']
-    azure_topic_namespace = data['azure']['topic']['namespace']
-    azure_topic_keyname = data['azure']['topic']['keyname']
-    azure_topic_key = data['azure']['topic']['key']
 
     def configure_logging(self, file_path, module_name):
         """
