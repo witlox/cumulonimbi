7dd28450d4adae7374b27e0c33a448dfc54a4cf7
Johannes Bertens <johannes.bertens@live.nl>
2015-09-10 21:02:41 +0200

    Cleared logic, need to add algorithms soon.

--- machine_manager/logic.py
+++ machine_manager/logic.py
@@ -1,12 +1,12 @@
 class MachineManagerLogic(object):
     def __init__(self):
-        self.jobs = []
+        pass
 
     def check_machines(self):
         pass
 
     def job_added(self, job_id):
-        self.jobs.append(job_id)
+        pass
 
     def job_removed(self, job_id):
-        self.jobs.remove(job_id)
+        pass
